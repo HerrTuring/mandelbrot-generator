b6c7cf9706a5a62fb47df0857343b5c793fbe4cd
Herr Turing <admin@nda.software>
2023-06-26 15:05:19 -0300

    Initial commit.

---
+++ MandelbrotGenerator/MandelbrotGenerator.h
@@ -0,0 +1,6 @@
+#pragma once
+
+int main();
+int loopingCalculator(double x, double z, int loopLimit);
+std::vector<int> getColor(int loopCount);
+void fillBitmap(int i, int j, std::vector<int> color);
